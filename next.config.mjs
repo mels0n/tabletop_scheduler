0d25bd5eb26cefc4105e7c9701741db6f33f2478
Chris Melson <chris.m.melson@gmail.com>
2026-01-19 16:30:50 -0600

    fix(config): disable instrumentation hook to resolve serverless runtime errors

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'standalone',
     experimental: {
-        instrumentationHook: true,
+        instrumentationHook: false,
     },
     webpack: (config, { webpack }) => {
 
